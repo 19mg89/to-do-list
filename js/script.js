84f2fc66bd8d983640e0d79f05a5ec811f7d490f
Marcin Gornik <m.gornik89@gmail.com>
2023-10-29 15:31:08 +0000

    done & remove button refurbished-decorations added

--- js/script.js
+++ js/script.js
@@ -43,13 +43,15 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li
-            ${task.done ? "style=\"text-decoration: line-through\"" : ""}
-            >
-            <button class="js-done">zrobione?</button>
-            <button class="js-remove">Usuń</button>
+            <li class="tasks__item">
+            <button class="tasks__button tasks__button--toggleDone js-done"> 
+            ${task.done ? "✓" : ""}
+            </button>
+           <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
             ${task.content} 
-            </li> 
+            </span>
+            <button class="js-remove tasks__button tasks__button--remove">🗑</button>
+            </li>
             `;
         }
 
